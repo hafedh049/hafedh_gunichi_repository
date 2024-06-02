7ef04916b91fc54760048ab0f1f7a5893c10e3e5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-06-02 18:43:42 +0100

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
